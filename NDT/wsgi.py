42bca83dd2a00b3f28d7d7f5ac70d6510b41ab0d
hernan neuman <hernanneuman15@hotmail.com>
2016-10-03 22:01:06 -0400

    """ ,, ,,,"""

--- NDT/wsgi.py
+++ NDT/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 """
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "han.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NDT.settings")
 """
 application = get_wsgi_application()
 
